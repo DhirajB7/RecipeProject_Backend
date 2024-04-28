6d321b7eb2b3c3bd690662f8f8f5738713f43aef
Dhiraj <dhiraj.basavaraju@gmail.com>
2024-04-28 10:26:17 +0530

    ++

--- src/main/java/com/dhirajb7/recipe/controller/Test.java
+++ src/main/java/com/dhirajb7/recipe/controller/Test.java
@@ -1,5 +1,9 @@
 package com.dhirajb7.recipe.controller;
 
+import com.dhirajb7.recipe.factory.StringToObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "test")
 public class Test {
-	
+
+	@Value("${environment}")
+	private String envName;
+
 	@GetMapping(path = "/")
-	public String test() {
-		return "working";
+	public ResponseEntity<StringToObject> test() {
+		return new ResponseEntity<>(new StringToObject("Working in env " + envName), HttpStatus.OK);
 	}
 
 }
---
+++ src/main/java/com/dhirajb7/recipe/factory/StringToObject.java
@@ -0,0 +1,11 @@
+package com.dhirajb7.recipe.factory;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@AllArgsConstructor
+@Data
+public class StringToObject {
+
+    private String message;
+
+}
